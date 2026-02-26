74c75ca6b73aae3339697b45b21aa783fe4838f3
Amhed Hussien <142429942+Eng-Ahmed-Hussien@users.noreply.github.com>
2026-02-26 06:22:20 +0200

    refactor: use LabRenderer in LabLayout and remove LabIframe

--- src/features/lab-session/components/LabLayout.tsx
+++ src/features/lab-session/components/LabLayout.tsx
@@ -1,12 +1,12 @@
 import { LabTopbar } from './LabTopbar';
-import { LabIframe } from './LabIframe';
+import { LabRenderer } from './LabRenderer';
 
 export const LabLayout = () => {
   return (
     <div className='flex flex-col h-screen w-screen overflow-hidden bg-background'>
       <LabTopbar />
       <main className='flex-1 relative'>
-        <LabIframe />
+        <LabRenderer />
       </main>
     </div>
   );
