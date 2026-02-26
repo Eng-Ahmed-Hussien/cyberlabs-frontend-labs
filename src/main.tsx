88623598a96cdbc0435474c6a56e9df33f19ec09
Eng-Ahmed-Hussien <Ahmed.hussien685@el-eng.menofia.edu.eg>
2026-02-26 02:15:36 +0200

    upload project files

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { AppProviders } from '@/core/providers';
+import App from './App';
+import './index.css';
+
+import '@/core/styles/globals.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <AppProviders>
+      <App />
+    </AppProviders>
+  </StrictMode>,
+);
---
+++ src/shared/constants/routes.ts
@@ -0,0 +1,5 @@
+export const ROUTES = {
+  HOME: '/',
+
+  UNAUTHORIZED: '/401',
+} as const;
